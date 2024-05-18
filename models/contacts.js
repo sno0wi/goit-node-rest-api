931a8e57a844f34d85dc8f70a103afe7be3ecf4f
sno0wi <sno0wi20@gmail.com>
2024-05-18 20:36:09 +0300

    add versionKey = false

--- models/contacts.js
+++ models/contacts.js
@@ -1,20 +1,25 @@
 import mongoose from "mongoose";
 
-const contactsSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    versionKey: false,
+  }
+);
 
 export default mongoose.model("Contact", contactsSchema);
